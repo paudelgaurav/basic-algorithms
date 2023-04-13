efedaf6c132e105109bb0be5c5dca29d49798822
Gaurav Paudel <gaurav.paudel@wesionary.team>
2023-04-13 21:43:30 +0545

    :sparkles: feat: added two crystal balls

--- two_crystal_balls/two_crystal_ball.go
+++ two_crystal_balls/two_crystal_ball.go
@@ -4,22 +4,26 @@ import (
 	"math"
 )
 
-// Two Crystal Balls
-// Find the closest point where the two balls will break
-// if they are droped from certain height
+// TwoCrystalBalls finds the closest point where two crystal balls will break
+// if they are dropped from a certain height. It takes an array of boolean values
+// as input, where each boolean value represents whether or not the crystal ball
+// breaks at that point. The function returns the index of the closest breaking point
+// of the two crystal balls, or -1 if both balls do not break.
+// Time complexity: O(sqrt(n)).
 func TwoCrystalBalls(breaks []bool) int {
 	// using square root as jmp point
 	jmpPoint := int(math.Floor(math.Sqrt(float64(len(breaks)))))
+	// Starting from the jump point and iterating through each jump point,
+	// find the first breaking point of one of the two crystal balls.
 	i := jmpPoint
 	for ; i < len(breaks); i += jmpPoint {
 		if breaks[i] {
 			break
 		}
 	}
-
+	// Backtrack to find the breaking point of the other crystal ball,
+	// iterating one element at a time.
 	i -= jmpPoint
-
-	// backtracking to find breaking point of another ball
 	for j := 0; j < jmpPoint && i < len(breaks); j, i = j+1, i+1 {
 		if breaks[i] {
 			return i
